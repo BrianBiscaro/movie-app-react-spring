f909a51d33cf159b33ee6a11d680e414bfa0fa26
Brian Biscaro <brianbiscaro22@gmail.com>
2026-02-24 00:10:41 -0300

    update external api movie service

---
+++ movie-react-app/src/services/externalApiMovieService.js
@@ -0,0 +1,18 @@
+import api from "./axiosConfig";
+
+const BASE_URL = api.BASE_URL
+export const popularMoviesService = async () => {
+
+    const response = await api.get(`${BASE_URL}/catalog`)
+    const data = await response.json();
+
+    return data.results;
+}
+
+export const searchMoviesService = async (query) => {
+
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json();
+
+    return data.results;
+}
